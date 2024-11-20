f44956414c20551598ebb7203b3ea8ce969bbdd9
Akus <akosbodor@gmail.com>
2024-11-20 21:05:19 +0100

    add my simple index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
-// Comment these lines to hide errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// My first PHP application
+echo "Hello World";
 
-require 'includes/config.php';
-require 'includes/functions.php';
-
-init();
+?>
